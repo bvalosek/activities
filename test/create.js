dbafa96491f07a73a05597da45a69129e8777d60
Brandon Valosek <bvalosek@gmail.com>
2014-01-22 13:33:26 -0600

    newIntent method

--- test/create.js
+++ test/create.js
@@ -1,5 +1,6 @@
+var Activity        = require('../lib/Activity.js');
 var ActivityManager = require('../lib/ActivityManager.js');
-var test = require('tape');
+var test            = require('tape');
 
 test('Bundle passed', function(t) {
   t.plan(3);
@@ -18,5 +19,36 @@ test('Bundle passed', function(t) {
   }
 
   m.start(A, null, BUNDLE);
+
+});
+
+test('New intent fired', function(t) {
+  t.plan(5);
+
+  var BUNDLE = {};
+
+  var m = new ActivityManager();
+
+  var create = 0;
+  var intent = 0;
+  function A()
+  {
+    this.onCreate = function(bundle) {
+      t.strictEqual(bundle, BUNDLE);
+      create++;
+    }
+
+    this.onNewIntent = function(bundle) {
+      t.strictEqual(bundle, BUNDLE);
+      intent++;
+    }
+  }
+
+  m.start(A, Activity.modes.SINGLE_INSTANCE, BUNDLE);
+  m.start(A, null, BUNDLE);
+  m.start(A, null, BUNDLE);
+
+  t.strictEqual(create, 1);
+  t.strictEqual(intent, 2);
 
 });
